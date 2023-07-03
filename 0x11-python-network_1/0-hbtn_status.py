57d03deecf8e106dd3819d70199d25402a8ff8db
chooday12 <chikefischer@gmail.com>
2023-07-02 21:08:22 -0700

    My 0x11-python-network_1 commit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-"""Fetch web response
+"""Fetch
+https://intranet.hbtn.io/status
+using urlib package
 """
 
 import urllib.request
 
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        content = response.read()
-    print('Body response:')
-    print('\t- type: {}'.format(type(content)))
-    print('\t- content: {}'.format(content))
-    print('\t- utf8 content: {}'.format(str(content)[2:-1]))
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as request:
+        response = request.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(response)))
+        print("\t- content: {}".format(response))
+        print("\t- utf8 content: {}".format(response.decode('UTF-8')))
